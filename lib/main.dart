dcae6584e8432816fd7f1a94d708cdd81e610b5d
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2026-01-03 00:53:32 +0000

    Update counter logic and add VSCode settings
    Changed the initial counter value to 10 and updated the increment logic to decrement the counter, displaying a message when it reaches zero. Also added a .vscode/settings.json file with custom cSpell words.

--- lib/main.dart
+++ lib/main.dart
@@ -54,16 +54,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 10;
+  String text = '';
 
   void _incrementCounter() {
     setState(() {
+      if (_counter == 0) {
+        text = 'Enough, Its zero motherfucker !';
+        return;
+      } else {
+        _counter--;
+        text = '$_counter';
+      }
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
@@ -105,10 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text(text, style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
