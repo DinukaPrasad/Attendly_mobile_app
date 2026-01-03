bf157473398328613459784bee4806d6ee7c7d5c
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2026-01-03 01:05:28 +0000

    Add simple Attendly scaffold

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,7 @@
+class AuthService {
+  // TODO: Implement login logic
+  Future<void> login(String email, String password) async {}
+
+  // TODO: Implement logout logic
+  Future<void> logout() async {}
+}
---
+++ lib/services/storage_service.dart
@@ -0,0 +1,7 @@
+class StorageService {
+  // TODO: Implement storage read
+  Future<String?> read(String key) async => null;
+
+  // TODO: Implement storage write
+  Future<void> write(String key, String value) async {}
+}
